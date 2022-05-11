c4933d0f2a5d1b1a34b430c60665f1a5b26565a8
unknown <ibrahimalouissa@gmail.com>
2022-05-11 13:11:13 +0200

    project init

---
+++ front-end/pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <navbar/>
+    <h1>test</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "index",
+  components: {}
+
+}
+</script>
+
+<style scoped>
+
+</style>
