cab1fd0c395576bad448d579110eaceac5a2ded0
unknown <ibrahimalouissa@gmail.com>
2022-05-11 16:36:10 +0200

    landing page img + transition

--- front-end/pages/index.vue
+++ front-end/pages/index.vue
@@ -1,7 +1,23 @@
 <template>
   <div>
-    <navbar/>
-    <h1>test</h1>
+    <div class="bg-teal-100">
+      <navbar/>
+      <div class="">
+        <div class="grid sm:grid-cols-2 gap-4">
+          <div class="m-12">
+            <h1 class="text-3xl md:text-4xl lg:text-7xl xl:text-7xl font-semibold"><span class="text-teal-400">Blink</span>  Helping you create a blink-worthy website</h1>
+            <p></p>
+          </div>
+          <div class="flex items-center justify-center">
+            <img src="/img/landingpage.svg" alt="" class="object-contain w-4/5 lg:1/5">
+          </div>
+        </div>
+      </div>
+    </div>
+    <img src="/img/transistionlayer2.svg" alt="" class="w-full">
+    <div>
+      
+    </div>
   </div>
 </template>
 
